771c4433e4560c221f3e37c808a754dfbb919690
Shayankhan28 <mshayankhan258@gmail.com>
2026-04-22 12:18:58 +0500

    add new data

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from main import add
 
 class testmain(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2,3),5)
+        self.assertEqual(add(2,3),52)
 
 
 if __name__ == "__main__":
